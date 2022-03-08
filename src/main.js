fbc7930aee8d4f70255c6060561055e5ff605ae2
Jaqueline de Oliveira <jaquelinedeoliveiraa@gmail.com>
2022-03-08 18:44:35 -0300

    cards países com total de atletas

--- src/main.js
+++ src/main.js
@@ -1,13 +1,31 @@
 import { example } from './data.js';
 import data from './data/athletes/athletes.js';
-console.log(example, data);
 
 
-/*const cartasPaíses = data.athletes.map((país)=>{
-  return `<div><p class="conteudo-países">${país.team}
-  </p> <p class="conteudo-países">${país.name}</p></div>`})
 
-console.log (cartasPaíses);
-document.getElementById("cards-países").innerHTML = cartasPaíses;*/
+const atletas = data.athletes
 
+const paises = atletas.reduce((valorAnterior, atletaAtual) => {
+  const pais = atletaAtual.team;
+  if (pais in valorAnterior) {
+    const atletasAnteriores = valorAnterior[atletaAtual.team];
+    return { ...valorAnterior, [pais]: [...atletasAnteriores, atletaAtual] }
+  }
+  else {
+    return { ...valorAnterior, [pais]: [atletaAtual] }
+  }
+}, {})
 
+let cartasPaíses = ""
+for (const pais in paises) {
+  cartasPaíses += `<div class="flex-item">
+  <ol class="background">
+  <li class="conteudo-países">${pais}</li>
+  <li class="conteudo-países">${paises[pais].length} atletas participantes</li>
+  </ol>
+  </div>`
+}
+
+console.log(cartasPaíses);
+
+document.getElementById("cards-países").innerHTML = cartasPaíses;
