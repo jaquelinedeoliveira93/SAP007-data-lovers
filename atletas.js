34659e73b35c5400852095a3760c3a1737b8fcc9
Jaqueline de Oliveira <jaquelinedeoliveiraa@gmail.com>
2022-03-28 18:36:30 -0300

    Updates

--- atletas.js
+++ atletas.js
@@ -1,14 +1,20 @@
-import { AZsort, filterAth, filterMedal, filterSport} from './data.js';
+import { AZsort, filterAth, filterMedal, filterSport } from './data.js';
 import data from './data/athletes/athletes.js';
 
 //Mostrando os cards na tela
 
-function showCards(data){
-const cartasAtletas = data.map((atletas)=>{
-    return `<div class="flex-item-atleta"><ol class = "cards-atletas"><li class="conteudo-atletas">${atletas.name}
-    </li> <li class="conteudo-atletas">${atletas.team}</li>
-    <li class="conteudo-atletas">${atletas.sport}</li>
-    <li class="conteudo-atletas">${atletas.medal}</li></ol></div>`})
+function showCards(data) {
+  const cartasAtletas = data.map((atletas) => {
+    return `
+      <div class="flex-item-atleta">
+        <ol class = "cards-atletas">
+          <li class="conteudo-atletas">${atletas.name}</li>
+          <li class="conteudo-atletas">${atletas.team}</li>
+          <li class="conteudo-atletas">${atletas.sport}</li>
+          <li class="conteudo-atletas">${atletas.medal}</li>
+        </ol>
+      </div>`
+  });
 
   document.getElementById("cards-atletas").innerHTML = cartasAtletas.join("");
 }
@@ -18,41 +24,55 @@ showCards(data.athletes)
 
 //Função para que o card apareça assim que o usuário escrever o nome do atleta
 
-document.getElementById("filterAth").addEventListener("keypress", changeFilter)
-function changeFilter(evento){
-const nome = evento.target.value
-const resultado = filterAth(data.athletes, nome)
-showCards(resultado)
+document.getElementById("filterAth").addEventListener("keyup", changeFilter)
+
+function changeFilter(evento) {
+  const nome = evento.target.value
+  const resultado = filterAth(data.athletes, nome)
+  showCards(resultado)
 }
+
 //Função para que o card apareça de acordo com o esporte escolhido
 
 document.getElementById("filterSport").addEventListener("change", changeFilterSport)
-function changeFilterSport(evento){
-const esporte = evento.target.value
-const resultadoEsporte = filterSport(data.athletes, esporte)
-showCards(resultadoEsporte)
+function changeFilterSport(evento) {
+  const esporte = evento.target.value
+  const resultadoEsporte = filterSport(data.athletes, esporte)
+  showCards(resultadoEsporte)
 }
+
 //Função para que o card apareça assim que clicar nas opções de A-Z ou Z-A
 
 document.getElementById("AZsort").addEventListener("change", changeSelect)
-function changeSelect(evento){
-const selectSort = evento.target.value
-const Resultsort = AZsort(data.athletes, selectSort)
-showCards(Resultsort)
+function changeSelect(evento) {
+  const selectSort = evento.target.value
+  const Resultsort = AZsort(data.athletes, selectSort)
+  showCards(Resultsort)
 }
 
 // Função para selecionar medalha
 
 document.getElementById("ordering-medal").addEventListener("change", selectMedal)
-function selectMedal (evento){
+function selectMedal(evento) {
   const medalhaSelect = evento.target.value
   const resultadoMedal = filterMedal(data.athletes, medalhaSelect)
   showCards(resultadoMedal)
 }
+
 //Botão limpar
 
 const clearOrderandFilter = document.getElementById("refresh");
-function clearAll(){
-  window.location.reload();
+function clearAll() {
+  FormData.reset();
 }
 clearOrderandFilter.addEventListener("click", clearAll);
+
+// Voltar ao topo
+
+const back = document.getElementById("backtoTop");
+
+back.addEventListener("click", function () {
+  window.scrollTo(0, 0);
+});
+
+
