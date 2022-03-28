a6dd478556ea002cb2f737c7964389611c8464d0
Larasantos97 <larissa_reis97@hotmail.com>
2022-03-28 18:16:37 -0300

    Alterações finais

--- src/data.js
+++ src/data.js
@@ -1,21 +1,21 @@
 
- //Filtro por nome de atleta
+//Filtro por nome de atleta
 
-  export const filterAth = (athletes, name) => {
-    return athletes.filter(atleta => {
-    return atleta.name.toLowerCase().includes (name.toLowerCase())
-    });
-  }
+export const filterAth = (athletes, name) => {
+  return athletes.filter(atleta => {
+    return atleta.name.toLowerCase().includes(name.toLowerCase())
+  });
+}
 //Filtro por medalha
 
-export const filterMedal =  (athletes, medal) => {
+export const filterMedal = (athletes, medal) => {
   return athletes.filter(medalha => {
     return medalha.medal === (medal)
   });
 }
 //Filtro por esporte
 
-export const filterSport =  (athletes, sport) => {
+export const filterSport = (athletes, sport) => {
   return athletes.filter(esporte => {
     return esporte.sport === (sport)
   });
@@ -24,61 +24,20 @@ export const filterSport =  (athletes, sport) => {
 //Ordenação de A-Z e Z-A
 
 export const AZsort = (athletes, selectSort) => {
-  if (selectSort === "A-Z"){
-    return athletes.sort((a, b) => {
-      const AlphabetA = a.name.toUpperCase();
-      const AlphabetZ = b.name.toUpperCase();
-      if (AlphabetA < AlphabetZ){
-        return -1;
-      }
-      if (AlphabetA > AlphabetZ){
-        return 1;
-      }
-      return 0;
 
-    });
-   } else {
-    return athletes.sort((a, b) => {
-      const AlphabetA = a.name.toUpperCase();
-      const AlphabetZ = b.name.toUpperCase();
-      if (AlphabetA < AlphabetZ){
-        return 1;
-      }
-      if (AlphabetA > AlphabetZ){
-        return -1;
-      }
-      return 0;
-
-    });
-   }
-  }
-
-
-export const sortCountry = (athletes, selectCountry) => {
-  if (selectCountry === "a-z"){
-    return athletes.sort((a, z) => {
-      const countryA = a.team;
-      const countryZ = z.team;
-      if (countryA < countryZ) {
-        return -1;
-      }
-      if (countryA > countryZ) {
-        return 1;
-      }
-      return 0;
-    });
+  if (selectSort === "A-Z") {
+    return athletes.sort((a, b) => (a.name.toUpperCase() > b.name.toUpperCase() ? 1 : -1))
   } else {
-    return athletes.sort((a, z) => {
-      const countryA = a.team;
-      const countryZ = z.team;
-      if (countryA < countryZ) {
-        return 1;
-      }
-      if (countryA > countryZ) {
-        return -1;
-      }
-      return 0;
-    });
+    return athletes.sort((a, b) => (a.name.toUpperCase() > b.name.toUpperCase() ? -1 : 1))
+  }
+};
+export const sortCountry = (athletes, selectCountry) => {
+
+  if (selectCountry === "a-z") {
+    return athletes.sort((a, b) => (a.team > b.team ? 1 : -1))
+  } else {
+    return athletes.sort((a, b) => (a.team > b.team ? -1 : 1))
   }
 }
+
 
